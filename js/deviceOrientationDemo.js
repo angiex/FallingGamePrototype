7ab4121d03c2d270af0c23546c674434d3dd6576
Angie Xiang <ufsor@student.kit.edu>
2019-12-17 11:17:10 +0100

    Cut off decimal points in deviceorientation demo

--- js/deviceOrientationDemo.js
+++ js/deviceOrientationDemo.js
@@ -1,7 +1,7 @@
 window.addEventListener("deviceorientation", (event) => {
-    let alpha = (event.alpha * 1000) / 1000;
-    let beta = (event.beta * 1000) / 1000;
-    let gamma = (event.gamma * 1000) / 1000;
+    let alpha = Math.sign(event.alpha) * Math.round(Math.abs(event.alpha) * 1000) / 1000;
+    let beta = Math.sign(event.beta) * Math.round(Math.abs(event.beta) * 1000) / 1000;
+    let gamma = Math.sign(event.gamma) * Math.round(Math.abs(event.gamma) * 1000) / 1000;
     document.getElementById("alpha").innerHTML = "Alpha: " + alpha;
     document.getElementById("beta").innerHTML = "Beta: " + beta;
     document.getElementById("gamma").innerHTML = "Gamma: " + gamma;
